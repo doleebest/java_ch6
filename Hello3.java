8fab4f22c9da3d05c5a3d70de995965897f8dbc3
doleebest <90204371+doleebest@users.noreply.github.com>
2022-01-26 23:43:19 +0900

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,11 @@
+//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+class Hello2 {} // 소스파일의 이름은 public class 이름과 일치
+class hello3 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+} // 하나의 소스파일에는 하나의 public class만 허용
+//소스파일 이름과 메인 함수가 있는 클래스 이름이 불일치하면 안됨 딴게 실행
+class Hello4 {}
+
+
